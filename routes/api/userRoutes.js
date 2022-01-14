caa37483d448febb9005578e737a7f4652da7471
Jason <LutzJason92@gmail.com>
2022-01-13 23:43:15 -0700

    Update userRoutes.js
    user Routes defined

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,21 +1,17 @@
 const router = require("express").Router();
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removefriend,
+} = require("../../controllers/userController.js");
 
-// GET all users
+router.route("/").get(getAllUsers).post(createUser);
 
-// GET a single user by its _id and populated thought and friend data
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// POST a new user:
-
-// PUT to update a user by its _id
-
-// DELETE to remove user by its _id
-
-// BONUS: Remove a user's associated thoughts when deleted.
-
-// /api/users/:userId/friends/:friendId
-
-// POST to add a new friend to a user's friend list
-
-// DELETE to remove a friend from a user's friend list
-
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removefriend);
 module.exports = router;
