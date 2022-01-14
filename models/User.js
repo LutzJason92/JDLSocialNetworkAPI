e0a4a3082a5e173cf3e0138ea0fb603595ce4083
Jason <LutzJason92@gmail.com>
2022-01-13 23:42:54 -0700

    Update User.js
    User model defined

--- models/User.js
+++ models/User.js
@@ -1,18 +1,45 @@
-// username
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
 
-// String
-// Unique
-// Required
-// Trimmed
-// email
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-// String
-// Required
-// Unique
-// Must match a valid email address (look into Mongoose's matching validation)
-// thoughts
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-// Array of _id values referencing the Thought model
-// friends
+const User = model("User", userSchema);
 
-// Array of _id values referencing the User model (self-reference)
+module.exports = User;
